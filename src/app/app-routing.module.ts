3b044a684e88af99cc4612ce4b546a92a533bbc6
habond <habondavid@gmail.com>
2019-08-28 20:29:07 +0200

    Books page styling. Create header.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,16 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HeaderComponent } from './header/header.component';
 
 
 const routes: Routes = [
-  { path: '', redirectTo: 'books', pathMatch: 'full' },
-  { path: '', loadChildren: 'src/app/pages/books/books.module#BooksModule' },
-  { path: '', loadChildren: 'src/app/pages/book-details/book-details.module#BookDetailsModule' },
-  { path: '', loadChildren: 'src/app/pages/new-book/new-book.module#NewBookModule' },
-  {
-    path: '**',
-    redirectTo: '/books'
-  }
+  { path: '', component: HeaderComponent, children: [
+    { path: '', redirectTo: 'books', pathMatch: 'full' },
+    { path: '', loadChildren: 'src/app/pages/books/books.module#BooksModule' },
+    { path: '', loadChildren: 'src/app/pages/book-details/book-details.module#BookDetailsModule' },
+    { path: '', loadChildren: 'src/app/pages/new-book/new-book.module#NewBookModule' },
+    {
+      path: '**',
+      redirectTo: '/books'
+    }
+  ]}
 ];
 
 @NgModule({
--- src/app/pages/books/books.module.ts
+++ src/app/pages/books/books.module.ts
@@ -2,14 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { BooksComponent } from './component/books.component';
 import { BooksRoutingModule } from './books-routing.module';
-
+import { MatButtonModule } from '@angular/material/button';
 
 
 @NgModule({
   declarations: [BooksComponent],
   imports: [
     CommonModule,
-    BooksRoutingModule
+    BooksRoutingModule,
+    MatButtonModule
   ]
 })
 export class BooksModule { }
