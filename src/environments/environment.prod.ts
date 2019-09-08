af879c4e6b46074cb8372bca22fd45a795e2e26c
habond <habondavid@gmail.com>
2019-09-08 17:32:31 +0200

    Remove firebase config

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -4,12 +4,6 @@ export const environment = {
   bookDetailsURL: 'https://www.googleapis.com/books/v1/volumes/',
   booksToReadURL: '/booksToRead',
   firebase: {
-    apiKey: 'AIzaSyCqUos_d1rRFGk0wD5wtHxalmZXqcCPNZc',
-    authDomain: 'example-bookshelf-app.firebaseapp.com',
-    databaseURL: 'https://example-bookshelf-app.firebaseio.com',
-    projectId: 'example-bookshelf-app',
-    storageBucket: '',
-    messagingSenderId: '6641015644',
-    appId: '1:6641015644:web:be18576ec192201b'
+    /** Your firebase config... */
   }
 };
